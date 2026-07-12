db8798ae173b6f73b06d153eb8ebd9d137b9ed52
Shady Mansour <Shadymansour19@gmail.com>
2026-07-12 22:45:50 +0300

    feature: add value search to CheckableComboBox

--- client/CheckableComboBox.py
+++ client/CheckableComboBox.py
@@ -1,8 +1,15 @@
-from PyQt6.QtCore import Qt, pyqtSignal
-from PyQt6.QtWidgets import QComboBox, QStyle, QStylePainter, QStyleOptionComboBox
+from PyQt6.QtCore import Qt, pyqtSignal, QSortFilterProxyModel
+from PyQt6.QtWidgets import QComboBox, QStyle, QStylePainter, QStyleOptionComboBox, QLineEdit
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
 
 _SELECT_ALL = "(Select All)"
+
+
+class _FilterProxyModel(QSortFilterProxyModel):
+    def filterAcceptsRow(self, source_row, source_parent):
+        if source_row == 0:
+            return True
+        return super().filterAcceptsRow(source_row, source_parent)
 
 
 class CheckableComboBox(QComboBox):
@@ -11,11 +18,31 @@ class CheckableComboBox(QComboBox):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._model = QStandardItemModel(self)
-        self.setModel(self._model)
+        self._proxy = _FilterProxyModel(self)
+        self._proxy.setSourceModel(self._model)
+        self._proxy.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        self.setModel(self._proxy)
         self._summary_text = "(All)"
         self.view().pressed.connect(self._handleItemPressed)
         self._skip_hide = False
         self._addSelectAllItem()
+
+        container = self.view().parentWidget()
+        self._searchEdit = QLineEdit(container)
+        self._searchEdit.setPlaceholderText("Search...")
+        self._searchEdit.setClearButtonEnabled(True)
+        self._searchEdit.textEdited.connect(self._onSearchTextChanged)
+        layout = container.layout()
+        layout.insertWidget(layout.indexOf(self.view()), self._searchEdit)
+
+    def showPopup(self):
+        super().showPopup()
+        self._searchEdit.clear()
+        self._searchEdit.setFocus()
+        container = self._searchEdit.parentWidget()
+        extra = self._searchEdit.sizeHint().height()
+        container.resize(container.width(), container.height() + extra)
+        self.view().resize(self.view().width(), self.view().height() + extra)
 
     def paintEvent(self, event):
         painter = QStylePainter(self)
@@ -33,13 +60,25 @@ class CheckableComboBox(QComboBox):
         item.setFlags(Qt.ItemFlag.ItemIsEnabled)
         self._model.appendRow(item)
 
+    def _onSearchTextChanged(self, text):
+        self._proxy.setFilterFixedString(text)
+        self._syncSelectAll()
+
+    def _visibleRows(self):
+        rows = []
+        for r in range(self._proxy.rowCount()):
+            source_row = self._proxy.mapToSource(self._proxy.index(r, 0)).row()
+            if source_row != 0:
+                rows.append(source_row)
+        return rows
+
     def _handleItemPressed(self, index):
-        row = index.row()
+        row = self._proxy.mapToSource(index).row()
         item = self._model.item(row)
         new_state = Qt.CheckState.Unchecked if item.checkState() == Qt.CheckState.Checked else Qt.CheckState.Checked
         if row == 0:
             item.setCheckState(new_state)
-            for i in range(1, self._model.rowCount()):
+            for i in self._visibleRows():
                 self._model.item(i).setCheckState(new_state)
         else:
             item.setCheckState(new_state)
@@ -49,9 +88,9 @@ class CheckableComboBox(QComboBox):
         self.filterChanged.emit()
 
     def _syncSelectAll(self):
+        visible = self._visibleRows()
         all_checked = all(
-            self._model.item(i).checkState() == Qt.CheckState.Checked
-            for i in range(1, self._model.rowCount())
+            self._model.item(i).checkState() == Qt.CheckState.Checked for i in visible
         )
         self._model.item(0).setCheckState(
             Qt.CheckState.Checked if all_checked else Qt.CheckState.Unchecked
@@ -70,6 +109,8 @@ class CheckableComboBox(QComboBox):
                 item = self._model.item(i)
                 if item.checkState() == Qt.CheckState.Unchecked:
                     prev_unchecked.add(item.text())
+        self._searchEdit.clear()
+        self._proxy.setFilterFixedString("")
         self._model.clear()
         self._addSelectAllItem()
         for text in sorted(texts) if sort else texts:
